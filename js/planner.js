6eb4ba99f9a5c8b385e70c3ea3ee033e7fa5e71c
wfdday <103098890+wfdday@users.noreply.github.com>
2026-07-13 21:08:01 +0300

    Create planner.js

---
+++ js/planner.js
@@ -0,0 +1,9 @@
+export function initPlanner() {
+  console.log('Планировщик этапов и календаря РФ инициализирован');
+  // Здесь будет полная логика из v1
+}
+
+export function calculateWorkdays(year, month) {
+  // Реализация календаря
+  return 22; // заглушка
+}
